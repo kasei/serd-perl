6ff717562734d691990e3aef539cdc33c2d17862
Gregory Todd Williams <greg@evilfunhouse.com>
2012-03-02 14:02:48 -0500

    Updated serd_config.h to be more portable.

--- serd_config.h
+++ serd_config.h
@@ -3,10 +3,18 @@
 #ifndef _SERD_CONFIG_H_WAF
 #define _SERD_CONFIG_H_WAF
 
+/*
 #define HAVE_FMAX 1
 #define HAVE_POSIX_MEMALIGN 1
-/* #undef HAVE_POSIX_FADVISE */
+#define HAVE_POSIX_FADVISE 1
 #define HAVE_FILENO 1
+*/
+
+#undef HAVE_FMAX
+#undef HAVE_POSIX_MEMALIGN
+#undef HAVE_POSIX_FADVISE
+#undef HAVE_FILENO
+
 #define SERD_VERSION "0.10.0"
 
 #endif /* _SERD_CONFIG_H_WAF */
